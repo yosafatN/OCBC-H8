e2e1c5abc83505375e2cd59c37be94f359c481a3
yosafatN <yosafatnababan77@gmail.com>
2021-10-15 11:25:49 +0700

    Pertemuan 9

---
+++ Sesi09/Kantor_WebAPI/Kantor_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kantor_WebAPI.Models;
+
+namespace Kantor_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetAllEmployee();
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems(string id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetEmployee(id);
+        }
+    }
+}
---
+++ Sesi09/Kantor_WebAPI/Kantor_WebAPI/Models/EmployeeContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Kantor_WebAPI.Models
+{
+    public class EmployeeContext
+    {
+        public string ConnectionString { get; set; }
+        public EmployeeContext (string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<EmployeeItem> GetAllEmployee ()
+        {
+            List<EmployeeItem> list = new();
+
+            using ( MySqlConnection conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new("SELECT * FROM `employee`", conn);
+                    using MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem()
+                        {
+                            Id = reader.GetInt32("id"),
+                            Nama = reader.GetString("nama"),
+                            JenisKelamin = reader.GetString("jenis_kelamin"),
+                            Alamat = reader.GetString("alamat")
+                        });
+                    }
+                }catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+            }
+            return list;
+        }
+
+        public List<EmployeeItem> GetEmployee(string id)
+        {
+            List<EmployeeItem> list = new();
+
+            using MySqlConnection conn = GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new("SELECT * FROM `employee` WHERE id = @id", conn);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    list.Add(new EmployeeItem()
+                    {
+                        Id = reader.GetInt32("id"),
+                        Nama = reader.GetString("nama"),
+                        JenisKelamin = reader.GetString("jenis_kelamin"),
+                        Alamat = reader.GetString("alamat")
+                    });
+                }
+            }
+            
+            return list;
+        }
+    }
+}
